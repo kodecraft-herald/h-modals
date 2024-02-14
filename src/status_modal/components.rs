62e111d05fe3de18fb884aa42fdd36fdf5f10efc
Oishh <MatienzoHerald@gmail.com>
2024-02-14 15:04:54 +0800

    Change Style

--- src/status_modal/components.rs
+++ src/status_modal/components.rs
@@ -11,23 +11,25 @@ pub fn StatusModal(
 ) -> impl IntoView
 {
     let status_class = match status {
-        Some(ComponentStatus::Info) => "info-modal-box",
-        Some(ComponentStatus::Success) => "success-modal-box",
-        Some(ComponentStatus::Neutral) => "neutral-modal-box",
-        Some(ComponentStatus::Warning) => "warning-modal-box",
-        Some(ComponentStatus::Error) => "error-modal-box",
-        _ => "neutral-modal-box",
+        Some(ComponentStatus::Info) => "modal-box bg-info rounded-box",
+        Some(ComponentStatus::Success) => "modal-box bg-success rounded-box",
+        Some(ComponentStatus::Neutral) => "modal-box bg-neutral rounded-box",
+        Some(ComponentStatus::Warning) => "modal-box bg-warning rounded-box",
+        Some(ComponentStatus::Error) => "modal-box bg-error rounded-box",
+        _ => "modal-box bg-neutral rounded-box",
     };
 
     view! {
 
         <Show when=move || signal.get() fallback=|| ()>
-            <div class="modal-cust-top blur-bg">
-                <div class={status_class}>
-                    <h3 class="font-bold text-2xl text-black">{title.clone()}</h3>
-                    <p class="py-4 text-black">{description.clone()}</p>
-                    <div class="modal-action">
-                        <button class="btn btn-sm rounded" title="Close" on:click = move |_| signal.set(false)>Close</button>
+            <div class = "blur-bg">
+                <div class="modal-top">
+                    <div class={status_class}>
+                        <h3 class="font-bold text-2xl text-black">{title.clone()}</h3>
+                        <p class="py-4 text-black">{description.clone()}</p>
+                        <div class="modal-action">
+                            <button class="btn btn-sm rounded" title="Close" on:click = move |_| signal.set(false)>Close</button>
+                        </div>
                     </div>
                 </div>
             </div>
