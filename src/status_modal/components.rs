e0ae01bb7d44db7e1c296fd4acda8e4d68128b50
Oishh <MatienzoHerald@gmail.com>
2024-02-16 12:47:25 +0800

    Add: Button Status and Text Color

--- src/status_modal/components.rs
+++ src/status_modal/components.rs
@@ -10,6 +10,10 @@ pub fn StatusModal(
     position: Option<Position>,
     #[prop(optional)]
     status: Option<ComponentStatus>,
+    #[prop(optional)]
+    button_status: Option<ComponentStatus>,
+    #[prop(optional)]
+    text_color: Option<ComponentStatus>,
     // #[prop(optional)]
     // custom_class: Option<String>,
 ) -> impl IntoView
@@ -23,10 +27,41 @@ pub fn StatusModal(
         _ => "modal-box bg-neutral rounded-box",
     };
 
+    let text_header_class = match text_color {
+        Some(ComponentStatus::Info) => "text-info",
+        Some(ComponentStatus::Success) => "text-success",
+        Some(ComponentStatus::Neutral) => "text-neutral",
+        Some(ComponentStatus::Warning) => "text-warning",
+        Some(ComponentStatus::Error) => "text-error",
+        _ => "text-black",
+    };
+
+    let text_desc_class = match text_color {
+        Some(ComponentStatus::Info) => "text-info-content",
+        Some(ComponentStatus::Success) => "text-success-content",
+        Some(ComponentStatus::Neutral) => "text-neutral-content",
+        Some(ComponentStatus::Warning) => "text-warning-content",
+        Some(ComponentStatus::Error) => "text-error-content",
+        _ => "text-black",
+    };
+
+    let button_class = match button_status {
+        Some(ComponentStatus::Info) => "btn btn-sm rounded btn-info",
+        Some(ComponentStatus::Success) => "btn btn-sm rounded btn-success",
+        Some(ComponentStatus::Neutral) => "btn btn-sm rounded",
+        Some(ComponentStatus::Warning) => "btn btn-sm rounded btn-warning",
+        Some(ComponentStatus::Error) => "btn btn-sm rounded btn-error",
+        _ => "btn btn-sm rounded",
+    };
+
     let position_class = match position {
-        Some(Position::TopLeft) => "modal-top",
+        Some(Position::TopLeft) => "modal-top-left",
+        Some(Position::TopMiddle) => "modal-top-middle",
+        Some(Position::TopRight) => "modal-top-right",
         Some(Position::Middle) => "modal-middle",
+        Some(Position::BottomLeft) => "modal-bottom-left",
         Some(Position::BottomMiddle) => "modal-bottom-middle",
+        Some(Position::BottomRight) => "modal-bottom-right",
         _ => "modal-top-middle",
     };
 
@@ -36,10 +71,10 @@ pub fn StatusModal(
             <div class = "blur-bg">
                 <div class={position_class}>
                     <div class={status_class}>
-                        <h3 class="font-bold text-2xl text-black">{title.clone()}</h3>
-                        <p class="py-4 text-black">{description.clone()}</p>
+                        <h3 class=format!("font-bold text-2xl {}", text_header_class)>{title.clone()}</h3>
+                        <p class=format!("py-4 {}", text_desc_class)>{description.clone()}</p>
                         <div class="modal-action">
-                            <button class="btn btn-sm rounded" title="Close" on:click = move |_| signal.set(false)>Close</button>
+                            <button class={button_class} title="Close" on:click = move |_| signal.set(false)>Close</button>
                         </div>
                     </div>
                 </div>
