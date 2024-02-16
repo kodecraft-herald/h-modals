c7c040284253cad46139d9aa4c118fb82012df33
Oishh <MatienzoHerald@gmail.com>
2024-02-16 15:08:32 +0800

    Add: Position to Confirm Modal

--- src/confirm_modal/components.rs
+++ src/confirm_modal/components.rs
@@ -1,5 +1,7 @@
 use leptos::{RwSignal, ReadSignal, IntoView, Show, view, component, SignalGet, SignalSet};
 
+use crate::attributes::enums::Position;
+
 #[allow(non_snake_case)]
 #[component]
 pub fn ConfirmModal<C>(
@@ -8,14 +10,27 @@ pub fn ConfirmModal<C>(
     description: String,
     mut function: C,
     pending_signal: ReadSignal<bool>,
+    #[prop(optional)]
+    position: Option<Position>,
 ) -> impl IntoView
 where C: FnMut() + Clone + 'static,
 {
+    let position_class = match position {
+        Some(Position::TopLeft) => "modal-top-left",
+        Some(Position::TopMiddle) => "modal-top-middle",
+        Some(Position::TopRight) => "modal-top-right",
+        Some(Position::Middle) => "modal-middle",
+        Some(Position::BottomLeft) => "modal-bottom-left",
+        Some(Position::BottomMiddle) => "modal-bottom-middle",
+        Some(Position::BottomRight) => "modal-bottom-right",
+        _ => "modal-middle",
+    };
+
     let on_click = move |_| function();
     view! {
         <Show when=move || signal.get() fallback=|| ()>
             <div class = "blur-bg">
-                <div class="modal-top">
+                <div class={position_class}>
                     <div class="modal-box rounded-box">
                         <h3 class="font-bold text-2xl">{title.clone()}</h3>
                         <p class="py-4">{description.clone()}</p>
